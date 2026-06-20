9aa7043782efac65db5aa5535ed973d5e706a853
Zhelair <nik.sales.737@gmail.com>
2026-06-20 22:20:17 +0300

    123
    123

--- api/account-status.js
+++ api/account-status.js
@@ -38,6 +38,9 @@ function toAccountSummary(account, deviceAccess, accessSync = {}) {
 
 export default async function handler(req, res) {
   setDefaultCorsHeaders(req, res)
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  res.setHeader('Pragma', 'no-cache')
+  res.setHeader('Expires', '0')
 
   if (req.method === 'OPTIONS') return res.status(200).end()
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' })
